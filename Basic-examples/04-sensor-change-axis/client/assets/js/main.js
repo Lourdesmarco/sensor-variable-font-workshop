7269149f7d5e41b662f72e443de0c24d62d67d73
Lourdes <lourdesmarco.design@gmail.com>
2019-10-24 14:22:07 +0200

    Add basic example 04 & arduino firmata

---
+++ Basic-examples/04-sensor-change-axis/client/assets/js/main.js
@@ -0,0 +1,27 @@
+window.onload = function() {
+    // get the title DOM element
+    let title = this.document.querySelector("#title");
+    
+    // get the input DOM element
+    let h1weight = this.document.querySelector("#h1weight");
+    
+    // listen the input change event
+    h1weight.addEventListener('input', function () {
+        // change the CSS property 
+        title.style.setProperty('--weightH1', h1weight.value);        
+    }, false);
+    
+    // Create Socket.io object
+    const socket = io();
+
+    // Listen server sensor values emit by socket.io
+    socket.on('sendNormalizedValue', function (normalizedValue, sensorValue) {
+
+        // get the subtitle DOM element
+        let subtitle = document.querySelector("#subtitle");
+        
+        // semantic function
+        subtitle.style.setProperty('--weightH2', normalizedValue);
+    })
+};
+
